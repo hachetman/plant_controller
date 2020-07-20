935a8f4aef9662df9eebdf8d0748a3ee002243b1
Christian Grumbein <christian@grumbein.de>
2020-07-20 21:00:52 +0200

    GPIO test on pump GPIO

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,19 @@
 void setup()
 {
   // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(0, OUTPUT);
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
 }
 
 void loop()
 {
   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(0, HIGH);
   // wait for a second
   delay(1000);
   // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(0, LOW);
    // wait for a second
   delay(1000);
 }
