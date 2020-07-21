aebcd1b16120877914a3b0bf595934254ab65fe3
Christian Grumbein <christian@grumbein.de>
2020-07-21 08:07:52 +0200

    Remove magic number

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@
 
 #include "Arduino.h"
 
+#define PUMP_GPIO 0
+
 void setup()
 {
   // initialize LED digital pin as an output.
-  pinMode(0, OUTPUT);
+  pinMode(PUMP_GPIO, OUTPUT);
   Serial.begin(115200);
   Serial.setTimeout(2000);
 }
@@ -17,11 +19,11 @@ void setup()
 void loop()
 {
   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(0, HIGH);
+  digitalWrite(PUMP_GPIO, HIGH);
   // wait for a second
   delay(1000);
   // turn the LED off by making the voltage LOW
-  digitalWrite(0, LOW);
+  digitalWrite(PUMP_GPIO, LOW);
    // wait for a second
   delay(1000);
 }
